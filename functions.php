38e691fb4da4a35f4d438ac60baeb4ed977ac47d
petschoice-owen <owen.caisip@petschoice.co.uk>
2022-11-17 16:32:07 +0800

    Cat Page - fixed styles on free shipping section

--- functions.php
+++ functions.php
@@ -376,3 +376,20 @@ function remove_gallery_thumbnail_images() {
 	}
 }
 add_action('loop_start', 'remove_gallery_thumbnail_images');
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Hide shipping rates when free shipping is available.
+/* Updated to support WooCommerce 2.6 Shipping Zones.
+/*-----------------------------------------------------------------------------------*/
+function my_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
